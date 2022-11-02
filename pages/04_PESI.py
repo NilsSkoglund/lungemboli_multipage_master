39ad8c274f1f63e38d0f772a139c99aaabe32c46
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2022-11-02 18:50:47 +0100

    Create 04_PESI.py

---
+++ pages/04_PESI.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.subheader("Röntgen")
+
+options = st.multiselect(
+    'Röntgensvar',
+    ['Alternativ 1', 'Alternativ 2', 'Alternativ 3'])
+
+st.write('You selected:', options)
+
+st.info("visa bilder här istället!")
