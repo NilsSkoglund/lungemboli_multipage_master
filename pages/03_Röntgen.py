74af0ecd1f77e761ffc5647b256e27eef7061c66
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2022-11-02 18:40:54 +0100

    Update 03_Röntgen.py

--- pages/03_Röntgen.py
+++ pages/03_Röntgen.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
 st.subheader("Röntgen")
+
+options = st.multiselect(
+    'What are your favorite colors',
+    ['Green', 'Yellow', 'Red', 'Blue'],
+    ['Yellow', 'Red'])
+
+st.write('You selected:', options)
