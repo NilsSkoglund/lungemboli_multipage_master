a18d9dc24102b66651194a66bafcc115dd594718
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2022-11-04 15:32:55 +0100

    .

--- pages/02_Ddimer.py
+++ pages/02_Ddimer.py
@@ -1,3 +1,24 @@
 import streamlit as st
 
 st.subheader("D-dimer")
+
+st.write("Fyll i Ålder och resultat från D-dimer testet.\
+     När du är färdig, markera rutan 'D-dimer klar'")
+
+
+st.number_input("Ange ålder",
+    step=1,
+    key="Ddimer_age"
+    )
+
+if st.session_state["Ddimer_age"]:
+    print("hej")
+
+
+st.number_input("Ange resultat D-dimer",
+    key="Ddimer_result"
+    )
+
+
+if st.session_state["ddimer_done"] == True:
+    Ddimer_display()
