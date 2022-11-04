1ad0a3db10f24b4d81722ee54f8be91c652868e9
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2022-11-04 14:33:41 +0100

    .

--- pages/01_PERC.py
+++ pages/01_PERC.py
@@ -12,9 +12,10 @@ dct_perc = {
     "Tidigare LE/DVT diagnos": 1,
     "Hjärtfrekvens >100/min": 1,
     "Hemoptys": 1,
-    "Immobiliserad i >3 dagar / Opererad senaste 4 veckor": 1.5,
-    "LE mer sannolik än annan diagnos": 3,
-    "Malignitet behandlad inom 6 mån eller palliation": 1
+    "Immobiliserad i >3 dagar / Opererad senaste 4 veckor": 1,
+    "Ålder ≥50": 1,
+    "Saturation >94% utan syrgas": 1,
+    "Östrogenbehandling": 1
     }
 name_perc = "perc"
 
