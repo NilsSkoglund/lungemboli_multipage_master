7b1db34000246f520aac1d4d9e5387d408ed980d
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2022-11-04 13:37:45 +0100

    Update 01_PERC.py

--- pages/01_PERC.py
+++ pages/01_PERC.py
@@ -1,31 +1,60 @@
 import streamlit as st
-from functions import f
-from streamlit_extras.switch_page_button import switch_page
 
 st.session_state.update(st.session_state)
-
 st.checkbox("test me!", value=st.session_state["test_01"], key="test_02")
 
+def calc_score(dct, name):
+	'''
+    Takes a dictionary (dct) and a string (name) as inputs and ...
+	calculates total score for:
+        DVT
+        PE
+        PERC
+        PESI
+	'''
+	total_score = 0
+	for index, question in enumerate(dct):
+		key = f"{name}_{index}"
+		if st.session_state[key]: # if True means the checkbox is ticked
+			total_score += dct.get(question)
+	return total_score
+
+dct_perc = {
+    "Kliniska tecken på DVT": 1,
+    "Tidigare LE/DVT diagnos": 1,
+    "Hjärtfrekvens >100/min": 1,
+    "Hemoptys": 1,
+    "Immobiliserad i >3 dagar / Opererad senaste 4 veckor": 1,
+    "Ålder ≥50": 1,
+    "Saturation >94% utan syrgas": 1,
+    "Östrogenbehandling": 1,
+    }
+
+name_perc = "perc"
+
+
 st.header("PERC")
-st.write("När du är färdig med formuläret, markera rutan 'PERC klar'")
 
 st.info("5 av 8 frågor i PERC ingår i Wells' PE formuläret.\
- När Wells' PE markeras som klar ges dessa frågor samma svar i PERC-formuläret.\
- Frågorna blir även låsta.")
-st.markdown("---")
+ När dessa frågor besvaras i Wells' PE formuläret \
+    ges de samma svar i PERC-formuläret.")
 
-f.create_checkboxes_perc(st.session_state["perc_dct"], st.session_state["perc_name"])
-
-st.markdown("---")
-st.checkbox("PERC klar", 
-    key="perc_mark_inside", 
-    value=st.session_state["perc_done"],
-    on_change=f.change_perc_in_to_out)
-if st.session_state["perc_done"] == True:
-    if calc_score(st.session_state["perc_dct"], st.session_state["perc_name"]) > 0:
-        st.warning("Inget av kriterierna får vara uppfyllt")
-        st.markdown("[Gå vidare till D-dimer](#d-dimer)")
-        
+for i, j in enumerate(dct_perc.items()):
+    if i < 5:
+        st.checkbox(
+            j[0],\
+            value=st.session_state[f"{name_lungemboli}_{i}"]\
+            key=f"{name_perc}_{i}")
     else:
-        st.success("PERC-regeln ej bruten, lungemboli kan uteslutas")
-        st.markdown("[Gå till Överblick](#verblick)")
+        st.checkbox(
+            j[0],\
+            key=f"{name_perc}_{i}")
+
+
+if calc_score(dct_perc, name_perc) > 0:
+    st.warning("Inget av kriterierna får vara uppfyllt")
+    st.markdown("[Gå vidare till D-dimer](#d-dimer)")
+    
+else:
+    st.success("PERC-regeln ej bruten, lungemboli kan uteslutas")
+    st.markdown("[Gå till Överblick](#verblick)")
