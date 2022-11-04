6c94787586eb676605590a64cbccff94f378acd1
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2022-11-04 17:06:29 +0100

    Update 03_Röntgen.py

--- pages/03_Röntgen.py
+++ pages/03_Röntgen.py
@@ -2,12 +2,25 @@ import streamlit as st
 
 st.session_state.update(st.session_state)
 
+# Initialize variables for radiology_report
+	# dct with report_answer:score
+	# name for key
+dct_radiology_report={
+	"Ingen lungemboli": 0,
+	"Lungemboli på subsegmentell nivå": 1,
+	"Lungemboli på segmentell nivå": 2,
+	"Lungemboli på lobär nivå": 3,
+	"Sadelemboli": 4
+	}
+
+name_rad = "dtla"
+
 st.header("Röntgen")
 
-options = st.multiselect(
-    'Röntgensvar',
-    ['Alternativ 1', 'Alternativ 2', 'Alternativ 3'])
+for i, j in enumerate(dct_radiology_report.items()):
+    st.checkbox(
+        j[0]\
+        , key=f"{name_rad}_{i}"
+    )
 
-st.write('You selected:', options)
-
-st.info("visa bilder här istället!")
+st.info("visa bilder här baserad på svar!")
