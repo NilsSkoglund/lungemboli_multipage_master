e41b6ea1e06b3e047eb45f0ed82b53fe1c14eba8
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2022-11-04 16:18:01 +0100

    Update 04_PESI.py

--- pages/04_PESI.py
+++ pages/04_PESI.py
@@ -36,4 +36,4 @@ for i, j in enumerate(dct_pesi.items()):
 
 st.markdown("---")
 st.metric("Totalpoäng PESI", value=f.calc_score(dct_pesi, name_pesi))
-#st.progess
+st.progess(f.calc_score(dct_pesi, name_pesi)/250)
