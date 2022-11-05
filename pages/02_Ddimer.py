30c1711f20cfd7c4618866e294f6501c5b8b4b59
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2022-11-05 07:36:46 +0100

    Update 02_Ddimer.py

--- pages/02_Ddimer.py
+++ pages/02_Ddimer.py
@@ -12,7 +12,7 @@ hide_streamlit_style = """
 """
 if st.checkbox('Remove padding'):
     st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+#st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
 ########################### Initialize Variables ##############################
 
