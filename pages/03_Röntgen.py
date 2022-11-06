afe45277c511e1e5d38f9ecb63b215267e528e96
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2022-11-06 18:18:48 +0100

    Update 03_Röntgen.py

--- pages/03_Röntgen.py
+++ pages/03_Röntgen.py
@@ -42,7 +42,7 @@ if st.session_state["dtla_0"]:
         """
         , unsafe_allow_html=True)
 
-    st.markdown('<a href="/Lungemboli" style="color: pink;" target="_self">KLAR</a>', unsafe_allow_html=True)
+    st.markdown('<a href="/Lungemboli" style="color: red; font-family:sans serif;" target="_self">KLAR</a>', unsafe_allow_html=True)
 
 verifierad_lungemboli = False
 
