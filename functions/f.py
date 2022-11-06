93dafea18d53ca769d23670cceafa3106afe9b8b
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2022-11-06 15:16:27 +0100

    Update f.py

--- functions/f.py
+++ functions/f.py
@@ -9,8 +9,35 @@ import base64
 ################################ Functions ####################################
 ############################# General functions ###############################
     # used for multiple different pages
+        # hide_anchor_link
         # calc_score
         # intialize_widget_keys
+def hide_anchor_link():
+    st.markdown(
+        body="""
+        <style>
+            h1 > div > a {
+                display: none;
+            }
+            h2 > div > a {
+                display: none;
+            }
+            h3 > div > a {
+                display: none;
+            }
+            h4 > div > a {
+                display: none;
+            }
+            h5 > div > a {
+                display: none;
+            }
+            h6 > div > a {
+                display: none;
+            }
+        </style>
+        """,
+         unsafe_allow_html=True,
+)
 
 def calc_score(dct, name):
 	'''
