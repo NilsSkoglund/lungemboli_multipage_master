ca569da277e1ca68e66b5d3d4e3dc888b367fb41
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2022-11-06 18:01:27 +0100

    Update 03_Röntgen.py

--- pages/03_Röntgen.py
+++ pages/03_Röntgen.py
@@ -34,7 +34,7 @@ st.warning("visa bilder här baserat på svar")
 
 if st.session_state["dtla_0"]:
     st.success("Lungemboli kan uteslutas. Överväg annan diagnos.")
-    st.write("[KLAR](https://nilsskoglund-lungemboli-multipage-master-lungemboli-gtesbp.streamlit.app/)")
+    st.markdown('<a href="/Lungemboli" target="_self">Next page</a>', unsafe_allow_html=True)
 
 verifierad_lungemboli = False
 
