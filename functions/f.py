64fdd8d68feeb2d03436bd4631803a9924cd45b1
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2022-11-07 21:51:19 +0100

    Update f.py

--- functions/f.py
+++ functions/f.py
@@ -117,6 +117,7 @@ def lungemboli_display_viz_v1(total_score):
 	'''
 	text_total_score = str(int(total_score*10))
     if text_total_score = "0":
+        st.write("hej")
         image = Image.open(f"img/t{text_total_score}.png")
         return st.image(image)
     else:
