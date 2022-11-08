03a5e8ad1ae47bf7ca70359c16de14cb9a7e01c9
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2022-11-08 12:53:56 +0100

    Update 05_Behandling.py

--- pages/05_Behandling.py
+++ pages/05_Behandling.py
@@ -1,4 +1,10 @@
 import streamlit as st
 
-with st.expander("Behandling 1\nLite text\nLite ny text"):
+with st.expander("Behandling 1"):
+    st.write("...")
+
+with st.expander("Behandling 2"):
+    st.write("...")
+
+with st.expander("Behandling 3"):
     st.write("...")
