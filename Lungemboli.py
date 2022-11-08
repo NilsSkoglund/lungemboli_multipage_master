299d0204b0bde957adcea8ffc238236e3ba17b8f
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2022-11-08 15:15:17 +0100

    Update Lungemboli.py

--- Lungemboli.py
+++ Lungemboli.py
@@ -79,9 +79,9 @@ button_style = """
         </style>
         """
 #st.markdown(button_style, unsafe_allow_html=True)
-with st.expander("Resultat", expanded=True):
+with st.expander("Riskvisualisering", expanded=True):
     f.lungemboli_display_viz_v1(st.session_state["total_score_pe"])
-    f.lungemboli_display_txt(st.session_state["total_score_pe"])
+f.lungemboli_display_txt(st.session_state["total_score_pe"])
 
 st.markdown("""
     <style>
