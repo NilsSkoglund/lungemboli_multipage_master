539fef23b9982f2c86b44d598429063dd5331f2d
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2022-11-08 22:51:08 +0100

    Update 04_PESI.py

--- pages/04_PESI.py
+++ pages/04_PESI.py
@@ -68,8 +68,7 @@ if pesi_score < 86:
         switch_page("Behandling")
 else:
     st.error("Hög risk. (gränsen för tillfället är satt till 86 men detta är arbiträrt för tillfället)")
-    css_example = '''
-    I'm importing the font-awesome icons as a stylesheet!                                                                                                                                                       
+    css_example = '''                                                                                                                                                    
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">                                                                                                    
 
     <i class="fa-solid fa-phone"></i> 
