9b111c1eac32b0da8eeffcbfb1b49e23007e94da
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2022-11-08 23:14:42 +0100

    Update Lungemboli.py

--- Lungemboli.py
+++ Lungemboli.py
@@ -12,10 +12,7 @@ f.hide_img_fullscreen()
 
 st.markdown("""
     <style>
-    [data-testid=stVerticalBlock]{row-gap: 0.1rem;}
-    [data-testid=stVerticalBlock]{gap: 0.1rem;}
-    .css-eca21k [data-testid=stVerticalBlock]{row-gap: 0.1rem;}
-    .css-1hrnv83 [data-testid=stVerticalBlock]{row-gap: 0.1rem;}
+    [data-testid=stVerticalBlock]{gap: 0.5rem;}
     </style>
     """, unsafe_allow_html=True)
 
