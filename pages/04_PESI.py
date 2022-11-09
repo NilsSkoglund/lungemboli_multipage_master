9828dee4728797889af83e5933675945c86e460e
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2022-11-09 17:38:31 +0100

    Update 04_PESI.py

--- pages/04_PESI.py
+++ pages/04_PESI.py
@@ -62,6 +62,7 @@ pesi_score += st.session_state["pesi_age"]
 #st.metric("Totalpoäng PESI", value=pesi_score)
 
 with st.expander("Riskvisualisering"):
+    pesi_score = min(180, pesi_score)
     img_string = str(pesi_score)
     image = Image.open(f"pages/img_pesi/pesi_{img_string}.png")
     st.image(image)
