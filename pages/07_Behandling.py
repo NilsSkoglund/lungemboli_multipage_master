7e3cffb493f36880207f330590b2a096fda60144
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2022-11-09 16:50:37 +0100

    Update 07_Behandling.py

--- pages/07_Behandling.py
+++ pages/07_Behandling.py
@@ -11,18 +11,10 @@ with st.expander("Behandling 2"):
 
 with st.expander("Behandling 3"):
     st.write("...")
-'''
-    <html>
-  <head>
-    <title>Title of the document</title>
-  </head>
-  <body>
-    <h1>Button with line break</h1>
-  </body>
-'''
+
 
 css_example = '''                                                                                                                                                    
         <button type="submit">My<br />Button</button>
 
     '''
-st.write(css_example, unsafe_allow_html=True)
+#st.write(css_example, unsafe_allow_html=True)
