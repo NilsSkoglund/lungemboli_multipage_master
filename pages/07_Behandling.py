a190128353fd155a3dac45dfde8cf458ed35ac97
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2022-11-09 13:17:48 +0100

    Update 07_Behandling.py

--- pages/07_Behandling.py
+++ pages/07_Behandling.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+st.markdown('<style>h1{color: red;}</style>', unsafe_allow_html=True)
+
 with st.expander("Behandling 1Behandling 1Behandling 1Behandling 1Behandling 1Behandling 1Behandling\
      1Behandling 1Behandling 1Behandling 1Behandling 1Behandling 1Behandling 1Behandling 1Behandling 1"):
     st.write("...")
@@ -9,3 +11,18 @@ with st.expander("Behandling 2"):
 
 with st.expander("Behandling 3"):
     st.write("...")
+'''
+    <html>
+  <head>
+    <title>Title of the document</title>
+  </head>
+  <body>
+    <h1>Button with line break</h1>
+  </body>
+'''
+
+css_example = '''                                                                                                                                                    
+        <button type="submit">My<br />Button</button>
+
+    '''
+st.write(css_example, unsafe_allow_html=True)
