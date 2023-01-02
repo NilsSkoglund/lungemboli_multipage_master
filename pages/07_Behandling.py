59b9f2ba19a5f777a7a3803c2b8be2b8edb97b86
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-01-02 12:41:19 +0100

    Update 07_Behandling.py

--- pages/07_Behandling.py
+++ pages/07_Behandling.py
@@ -6,15 +6,29 @@ with st.expander("Behandling 1"):
     st.header("Rukrik")
     st.subheader("Underrubrik")
     st.write("Brödtext...")
-    mer_info = st.checkbox("Visa mer info")
-    if mer_info: 
+    mer_info_1 = st.checkbox("Visa mer info")
+    if mer_info_1: 
         st.header("Rukrik")
         st.subheader("Underrubrik")
         st.write("Brödtext...")
 
 with st.expander("Behandling 2"):
-    st.write("...")
+    st.header("Rukrik")
+    st.subheader("Underrubrik")
+    st.write("Brödtext...")
+    mer_info_2 = st.checkbox("Visa mer info")
+    if mer_info_2: 
+        st.header("Rukrik")
+        st.subheader("Underrubrik")
+        st.write("Brödtext...")
 
 with st.expander("Behandling 3"):
-    st.write("...")
+    st.header("Rukrik")
+    st.subheader("Underrubrik")
+    st.write("Brödtext...")
+    mer_info_3 = st.checkbox("Visa mer info")
+    if mer_info_3: 
+        st.header("Rukrik")
+        st.subheader("Underrubrik")
+        st.write("Brödtext...")
 
