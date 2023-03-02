adcab65c188b07d070fbded9035e5c971c6e1d6d
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-03-02 12:12:03 +0100

    Update 07_Behandling.py

--- pages/07_Behandling.py
+++ pages/07_Behandling.py
@@ -8,56 +8,88 @@ st.markdown(
 """
 **Dos:** 200 E/kg/dygn
 
-**Att tänka på:** överkänsliget, vikt, koagulationsrubbning, HIT
+**Att tänka på:** Överkänslighet, vikt, koagulationsrubbning, HIT
 """
 )
 with st.expander("Visa kontraindikationer", expanded=False):
     st.markdown(
-"""
-**Absoluta kontraindikationer**
-- Ti överkänslighet för Fragmin
-"""
+    """
+    **Absoluta kontraindikationer**
+    - Ti överkänslighet för Fragmin
+    """
 )
     st.write("")
     st.markdown(
+    """
+    **Relativa ki**
+    - Pågående blödning
+    - Tidigare HIT typ II
+    - Svår koagulationsrubbning
+    """
+    )
+st.write("---")
+
+## Eliquis
+st.write("---")
+st.header("Eliquis (apixaban)")
+st.markdown(
 """
-**Relativa ki**
-- Pågående blödning
-- Tidigare HIT typ II
-- Svår koagulationsrubbning
+**Dos:** 10 mg x 2 i 7 dagar, därefter 5 mg x 2
+
+**Att tänka på:** Överkänsliget, njurfunktion, leversjukdom,\
+     blödning, annan ak behandlig, obesitas op.
 """
+)
+with st.expander("Visa kontraindikationer", expanded=False):
+    st.markdown(
+    """
+    **Absoluta kontraindikationer:**
+    - Ti överkänslighet Eliquis
+    """
+)
+    st.write("")
+    st.markdown(    
+    """
+    **Relativa ki:**
+    - Graviditet
+    - Pågående blödning
+    - Leversjukdom associerad med koagulationsrubbning
+    - Tillstånd med ökad blödningsrisk såsom esofagus varicer
+    - Svår koagulationsrubbning
+    - Ti obesitasoperation
+    - Samtidig behandling med annat antikoagulantium
+    """
 )
 st.write("---")
 
-with st.expander("Eliquis (apixaban)", expanded=True):
-    st.header("Eliquis (apixaban)")
-    st.markdown(
-"""
-**Dos:**
-- 10 mg x 2 i 7 dagar, därefter 5 mg x 2
-"""
-)
-    st.write("")
-    st.markdown(
-"""
-**Absoluta kontraindikationer:**
-- Ti överkänslighet Eliquis
-"""
-)
-    st.write("")
-    st.checkbox("Visa mer info", key = "mer_info_2")
-    if st.session_state["mer_info_2"]: 
-        st.markdown(
-"""
-**Relativa ki:**
-- Graviditet
-- Pågående blödning
-- Leversjukdom associerad med koagulationsrubbning
-- Tillstånd med ökad blödningsrisk såsom esofagus varicer
-- Svår koagulationsrubbning
-- Ti obesitasoperation
-- Samtidig behandling med annat antikoagulantium
-"""
-)
+# with st.expander("Eliquis (apixaban)", expanded=True):
+#     st.markdown(
+# """
+# **Dos:**
+# - 10 mg x 2 i 7 dagar, därefter 5 mg x 2
+# """
+# )
+#     st.write("")
+#     st.markdown(
+# """
+# **Absoluta kontraindikationer:**
+# - Ti överkänslighet Eliquis
+# """
+# )
+#     st.write("")
+#     st.checkbox("Visa mer info", key = "mer_info_2")
+#     if st.session_state["mer_info_2"]: 
+#         st.markdown(
+# """
+# **Relativa ki:**
+# - Graviditet
+# - Pågående blödning
+# - Leversjukdom associerad med koagulationsrubbning
+# - Tillstånd med ökad blödningsrisk såsom esofagus varicer
+# - Svår koagulationsrubbning
+# - Ti obesitasoperation
+# - Samtidig behandling med annat antikoagulantium
+# """
+# )
 
 
