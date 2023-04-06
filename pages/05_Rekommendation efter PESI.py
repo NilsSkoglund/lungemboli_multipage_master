f687bdb34d5f7f18eba28cdbc877788529ddf058
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-04-06 16:47:16 +0200

    Update 05_Rekommendation efter PESI.py

--- pages/05_Rekommendation efter PESI.py
+++ pages/05_Rekommendation efter PESI.py
@@ -96,71 +96,10 @@ with st.expander("Riskgrupp 2", expanded=expand_recommendation(65, 86)):
         Riskstratifieras enligt PESI i riskgrupp 2 där den genomsnittliga 30\
         dagars mortaliteten är mellan 1.7-3.5%"
     st.info(info_msg)
-    # Define custom CSS
-    custom_css = """
-    <style>
-    /* Custom CSS for Streamlit columns */
-
-    .css-ocqkz7.e1tzin5v4 {
-    display: flex !important;
-    flex-wrap: nowrap !important;
-    }
-
-    .css-ocqkz7.e1tzin5v4 > div {
-    flex: 1 !important;
-    min-width: 0 !important;
-    }
-    </style>
-    """
-
-    # Add custom CSS to the application
-    st.markdown(custom_css, unsafe_allow_html=True)
-
-    st.subheader("Vård")
-
-    col1, col2 = st.columns([1,3])
-
-    # Adjust column widths based on custom CSS
-    st.markdown(
-        f"<style>.css-ocqkz7.e1tzin5v4 > div:nth-child(1) {{ flex: 1 !important; }} .css-ocqkz7.e1tzin5v4 > div:nth-child(2) {{ flex: 2 !important; }}</style>",
-        unsafe_allow_html=True,
-    )
-
-    with col1:
-        st.write("Vårdnivå 1:")
-        st.write("Vårdnivå 2:")
-        st.write("Telemetrik:")
-        st.write("News:")
-        st.write("Behandling:")
-
-    with col2:
-        vård1 = st.radio("Vårdnivå 1:", options=["Hem", "Inläggning"], index=1, horizontal=True, label_visibility="collapsed")
-        if vård1 == "Hem":
-            is_hem = True
-        else:
-            is_hem = False
-        st.radio("Vårdnivå 2:", options=["Avdelning", "MIMA", "IVA"], index=0, horizontal=True, label_visibility="collapsed", disabled=is_hem)
-        st.radio("Telemetri:", options=["Ja", "Nej"], index=0, horizontal=True, label_visibility="collapsed", disabled=is_hem)
-        st.radio("News:", options=["x4", "x6", "x8"], index=1, horizontal=True, label_visibility="collapsed", disabled=is_hem)
-        st.radio("Behandling:", options=["Fragmin", "Eliquis"], index=0, horizontal=True, label_visibility="collapsed")
+    display_recommendations(2)
     
-    st.subheader("Remiss")
-
-    col11, col22 = st.columns([1,3])
-
-    with col11:
-        st.write("Hjärteko")
-        st.write("Koag. mottagn.")
-        st.write("Vårdcentral")
-
-    with col22:
-        st.radio("Hjärteko:", options=["Ja", "Nej"], index=1, horizontal=True, label_visibility="collapsed")
-        st.radio("Koagulationsmottagning:", options=["Ja", "Nej"], index=0, horizontal=True, label_visibility="collapsed")
-        st.radio("Vårdcentral:", options=["Ja", "Nej"], index=1, horizontal=True, label_visibility="collapsed")
 
 with st.expander("Riskgrupp 3", expanded=expand_recommendation(85, 106)):
-    st.subheader("Rekommendation för riskgrupp 3")
-
     st.info("**30 dagars mortalitet:** 3.2-7.1%")
     display_recommendations(3)
     
