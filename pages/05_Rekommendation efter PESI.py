117f0820383f0d6201e66f67d12fca4f74600539
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-04-06 16:02:28 +0200

    Update 05_Rekommendation efter PESI.py

--- pages/05_Rekommendation efter PESI.py
+++ pages/05_Rekommendation efter PESI.py
@@ -57,8 +57,10 @@ with st.expander("Riskgrupp 2", expanded=expand_recommendation(65, 86)):
         st.write("Behandling:")
 
     with col2:
-        st.radio("Vårdnivå 1:", options=["Hem", "Inläggning"], index=1, horizontal=True, label_visibility="collapsed")
-        st.radio("Vårdnivå 2:", options=["Avdelning", "MIMA", "IVA"], index=0, horizontal=True, label_visibility="collapsed")
+        vård1 = st.radio("Vårdnivå 1:", options=["Hem", "Inläggning"], index=1, horizontal=True, label_visibility="collapsed")
+        if vård1 == "Hem":
+            is_hem = True
+        st.radio("Vårdnivå 2:", options=["Avdelning", "MIMA", "IVA"], index=0, horizontal=True, label_visibility="collapsed", disabled=is_hem)
         st.radio("Telemetri:", options=["Ja", "Nej"], index=0, horizontal=True, label_visibility="collapsed")
         st.radio("News:", options=["x4", "x6", "x8"], index=1, horizontal=True, label_visibility="collapsed")
         st.radio("Behandling:", options=["Fragmin", "Eliquis"], index=0, horizontal=True, label_visibility="collapsed")
