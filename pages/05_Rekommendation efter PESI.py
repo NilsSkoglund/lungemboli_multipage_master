05174156801348b128be2835612c4f0d0a214abd
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-04-06 16:31:15 +0200

    Update 05_Rekommendation efter PESI.py

--- pages/05_Rekommendation efter PESI.py
+++ pages/05_Rekommendation efter PESI.py
@@ -24,11 +24,7 @@ if "pesi_score" not in st.session_state:
 st.write("Pesi score:", st.session_state["pesi_score"])
 
 def expand_recommendation(lower_limit, upper_limit):
-
     return lower_limit < st.session_state["pesi_score"] < upper_limit
-
-st.write("Hej")
-
 
 with st.expander("Riskgrupp 1", expanded=expand_recommendation(0, 66)):
     st.write("...")
