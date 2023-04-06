a04a99dca55f2cb23de8a0ee9054f002e3313109
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-04-06 13:02:07 +0200

    Update 05_Rekommendation efter PESI.py

--- pages/05_Rekommendation efter PESI.py
+++ pages/05_Rekommendation efter PESI.py
@@ -34,7 +34,7 @@ with st.expander("Riskgrupp 3", expanded=expand_recommendation(85, 106)):
         st.button("Inläggning")
         st.button("MIMA")
     with col4:
-        st.button("")
+        st.button("N/A", disabled=True)
         st.button("IVA")
     
 
