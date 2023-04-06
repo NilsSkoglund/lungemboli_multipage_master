4448f91c234ee4d3686feb2c20b0c76fd39605c7
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-04-06 15:37:01 +0200

    Update 05_Rekommendation efter PESI.py

--- pages/05_Rekommendation efter PESI.py
+++ pages/05_Rekommendation efter PESI.py
@@ -37,6 +37,13 @@ with st.expander("Riskgrupp 2", expanded=expand_recommendation(65, 86)):
     st.markdown(custom_css, unsafe_allow_html=True)
 
     col1, col2 = st.columns([1,3])
+
+    # Adjust column widths based on custom CSS
+    st.markdown(
+        f"<style>.css-ocqkz7.e1tzin5v4 > div:nth-child(1) {{ flex: 1 !important; }} .css-ocqkz7.e1tzin5v4 > div:nth-child(2) {{ flex: 2 !important; }}</style>",
+        unsafe_allow_html=True,
+    )
+
     with col1:
         st.write("Vårdnivå 1:")
         st.write("Vårdnivå 2:")
