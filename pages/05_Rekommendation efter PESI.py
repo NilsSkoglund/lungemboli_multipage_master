74be0077f343df4f3f3ef836ac0bc6f8dc6c90b1
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-04-06 12:14:28 +0200

    Update 05_Rekommendation efter PESI.py

--- pages/05_Rekommendation efter PESI.py
+++ pages/05_Rekommendation efter PESI.py
@@ -3,3 +3,18 @@ import streamlit as st
 st.header("Rekommendation efter PESI")
 
 st.write("Pesi score:", st.session_state["pesi_score"])
+
+def expand_recommendation(lower_limit, upper_limit):
+
+    return lower_limit < st.session_state["pesi_score"] < upper_limit
+
+
+st.expander("Riskgrupp 1", expanded=expand_recommendation(0, 66))
+
+st.expander("Riskgrupp 2", expanded=expand_recommendation(65, 86))
+
+st.expander("Riskgrupp 3", expanded=expand_recommendation(85, 106))
+
+st.expander("Riskgrupp 4", expanded=expand_recommendation(105, 126))
+
+st.expander("Riskgrupp 3", expanded=expand_recommendation(125, 300))
