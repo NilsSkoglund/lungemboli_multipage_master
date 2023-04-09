c6b08c27691448e20eac62031e6c8470614b28fa
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-04-09 12:59:10 +0200

    Update f.py

--- functions/f.py
+++ functions/f.py
@@ -138,7 +138,6 @@ def display_flow_v2():
     if st.session_state["total_score_pe"] < 2 and "wells_påbörjad" in st.session_state:
     # if low
         img_path = "låg/låg"
-        #img_path = "låg_alt"
         # check if PERC has been broken
         if calc_score(dct_perc, name_perc) > 0:
             img_path = "låg/låg"
@@ -150,11 +149,11 @@ def display_flow_v2():
             if st.session_state["Ddimer_status"] == "negative":
                 img_path = "låg/låg_broken_negative"
             # check röntgen
-            if "verifierad_lungemboli" in st.session_state:
-                if st.session_state["verifierad_lungemboli"] == False:
+            if "dtla_0" in st.session_state:
+                if st.session_state["dtla_0"] == True:
                     img_path = "låg/låg_broken_positive_ingen"
-                elif st.session_state["verifierad_lungemboli"] == True:
-                    img_path = "låg/låg_broken_positive_verified"
+            if st.session_state["verifierad_lungemboli"] == True:
+                img_path = "låg/låg_broken_positive_verified"
 
     elif 2 < st.session_state["total_score_pe"] < 6.5:
         img_path = "måttlig/måttlig"
