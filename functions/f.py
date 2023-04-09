586c65af0db0ee6e57d992df15bf52522de6bea3
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-04-09 15:47:48 +0200

    Update f.py

--- functions/f.py
+++ functions/f.py
@@ -176,13 +176,11 @@ def display_flow_v2():
             img_path = "måttlig/måttlig_negative"
         if st.session_state["Ddimer_status"] == "positive":
             img_path = "måttlig/måttlig_positive"
-            st.write("hej1")
-        if "dtla_0" in st.session_state:
-            if st.session_state["dtla_0"] == True:
-                img_path = "måttlig/måttlig_positive_ingen"
-        if st.session_state["verifierad_lungemboli"] == True:
-            st.write("hej2")
-            img_path = "måttlig/måttlig_positive_verified"
+            if "dtla_0" in st.session_state:
+                if st.session_state["dtla_0"] == True:
+                    img_path = "måttlig/måttlig_positive_ingen"
+            if st.session_state["verifierad_lungemboli"] == True:
+                img_path = "måttlig/måttlig_positive_verified"
         
             
     elif st.session_state["total_score_pe"] > 6:
