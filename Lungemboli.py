4d670565c410aacb585e8d601f9d33d2318fbcfc
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-04-18 11:15:41 +0200

    .

--- Lungemboli.py
+++ Lungemboli.py
@@ -1,5 +1,18 @@
 import streamlit as st
 from functions import f
+from st_pages import Page, show_pages, add_page_title
+
+# Optional -- adds the title and icon to the current page
+add_page_title()
+
+# Specify what pages should be shown in the sidebar, and what their titles and icons
+# should be
+show_pages(
+    [
+        Page("Lungemboli.py", "Home"),
+        Page("pages/01_PERC.py", "Page 2"),
+    ]
+)
 
 st.session_state.update(st.session_state)
 
