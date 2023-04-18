4b89d68d34505f0581132b003d5b025bdd2732b5
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-04-18 11:51:32 +0200

    Update Lungemboli.py

--- Lungemboli.py
+++ Lungemboli.py
@@ -3,7 +3,7 @@ from functions import f
 from st_pages import Page, show_pages, add_page_title
 
 with st.sidebar:
-    st.radio(["Svenska", "English"])
+    st.radio(label="Language", options=["Svenska", "English"])
 # Specify what pages should be shown in the sidebar, and what their titles and icons
 # should be
 show_pages(
