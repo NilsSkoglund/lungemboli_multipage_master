e975c7ae51cf65a0578b6d55556aa37396e99c5d
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-04-18 13:35:30 +0200

    Update 02_D-dimer.py

--- pages/02_D-dimer.py
+++ pages/02_D-dimer.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from functions import f
 from streamlit_extras.switch_page_button import switch_page
-
+from st_pages import Page, show_pages, hide_pages
 
 f.button_style()
 f.hide_anchor_link()
@@ -10,6 +10,27 @@ f.hide_hamburger()
 f.hide_padding_top()
 
 st.session_state.update(st.session_state)
+
+############################# language choice #################################
+if "lang" not in st.session_state:
+    st.session_state["lang"] == "Svenska"
+
+if st.session_state["lang"] == "Svenska":
+    hide_pages(["X-ray"])
+    result_header = "Ange resultat från D-dimer"
+    neg_txt = f"Negativt D-dimer test. Lungemboli kan uteslutas.\
+         Överväg annan diagnos."
+    pos_txt = f"Positivt D-dimer test. Det går ej att utesluta lungemboli.\
+          Fortsätt utredning med DTLA."
+else:
+    hide_pages(["Röntgen"])
+    result_header = "Enter D-dimer result"
+    neg_txt = f"Negative D-dimer test. Pulmonary embolism can be ruled out.\
+         Consider an alternative diagnosis."
+    pos_txt = f"Positive D-dimer test. Pulmonary embolism cannot be ruled out.\
+          Continue the investigation with CTPA."
+
+
 
 ########################### Initialize Variables ##############################
 
@@ -25,7 +46,7 @@ st.header("D-dimer")
 # if "Ddimer_result" not in st.session_state:
 #     st.session_state["Ddimer_result"] = 0.5
 
-st.number_input("Ange resultat från D-dimer"
+st.number_input(result_header
     , step=0.01
     , key="Ddimer_result"
     #, on_change=ddimer_update_slider
@@ -35,13 +56,12 @@ st.number_input("Ange resultat från D-dimer"
 # om man har fyllt i resulatunder 0.5 --> presentera slutsats
 if 0 < st.session_state["Ddimer_result"] <= 0.5:
     st.session_state["Ddimer_status"] = "negative"
-    st.success(f"Negativt D-dimer test. Lungemboli kan uteslutas.\
-         Överväg annan diagnos.")
+    st.success(neg_txt)
     f.klar()
 
 elif st.session_state["Ddimer_result"] >= 1.0:
     st.session_state["Ddimer_status"] = "positive"
-    st.error(f"Positivt D-dimer test. Det går ej att utesluta lungemboli. Fortsätt utredning med DTLA.")
+    st.error(pos_txt)
 
     col1, col2 = st.columns([1, 1])
     f.col_control_rem()
@@ -77,7 +97,7 @@ if 0.5 < st.session_state["Ddimer_result"] < 1.0:
 if "Ddimer_age" in st.session_state and 0.5 < st.session_state["Ddimer_result"] < 1.0:
     if st.session_state["Ddimer_result"] > st.session_state["beslutsgräns"]:
         st.session_state["Ddimer_status"] = "positive"
-        st.error(f"Positivt D-dimer test. Det går ej att utesluta lungemboli. Fortsätt utredning med DTLA.")
+        st.error(pos_txt)
 
         col1, col2 = st.columns([1, 1])
         f.col_control_rem()
@@ -88,8 +108,7 @@ if "Ddimer_age" in st.session_state and 0.5 < st.session_state["Ddimer_result"] 
                 switch_page("Röntgen")
     else:
         st.session_state["Ddimer_status"] = "negative"
-        st.success(f"Negativt D-dimer test. Lungemboli kan uteslutas.\
-         Överväg annan diagnos.")
+        st.success(neg_txt)
         f.klar()
 
 ############################ Flow Illustration  ###############################
