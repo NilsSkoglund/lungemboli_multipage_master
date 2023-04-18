1c2847e58b98b07a2c6e21afa75d611add049179
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-04-18 11:48:50 +0200

    Update Lungemboli.py

--- Lungemboli.py
+++ Lungemboli.py
@@ -2,9 +2,8 @@ import streamlit as st
 from functions import f
 from st_pages import Page, show_pages, add_page_title
 
-# Optional -- adds the title and icon to the current page
-add_page_title()
-
+with st.sidebar:
+    st.write("hej")
 # Specify what pages should be shown in the sidebar, and what their titles and icons
 # should be
 show_pages(
