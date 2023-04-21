c9da1a711f3a93819b4c9cfeaa978e63951b79ea
NilsSkoglund <69119610+NilsSkoglund@users.noreply.github.com>
2023-04-21 08:27:29 +0200

    Update Lungemboli.py

--- Lungemboli.py
+++ Lungemboli.py
@@ -137,4 +137,8 @@ else:
 
 ############################ Flow Illustration  ###############################
 with st.sidebar:
-    f.display_flow_v2()
+    if st.session_state["lang"] == "English":
+        f.display_flow_v2_eng()
+    else:
+        f.display_flow_v2()
+    
